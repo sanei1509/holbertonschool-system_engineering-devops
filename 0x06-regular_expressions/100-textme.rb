2feaf769a715a67fd401288fb68d5b7e7bbdb1eb
Santiago Neira <4373@holbertonschool.com>
2022-06-22 06:20:10 +0000

    try fix the task 100 advanced

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 #obtener de los registros de inicio de sesion solo:
 #[el que envía(num)] [el que recibe(num)] [flags usadas]
-#
 
-puts ARGV[0].scan(/(?<=from:|to:|flags:).join(',')
+puts ARGV[0].scan(/(?<=from:|to:|flags:)(.+?)(?=\])/).join(",")
