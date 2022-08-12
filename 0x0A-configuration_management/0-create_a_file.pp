9b86fdb0fa5b132f12f18dac89143ed5c805ac33
Santiago Neira <santiagoneira52@gmail.com>
2022-08-12 14:00:14 +0000

    fix puppet lint and correct output

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Crear un archivo en /tmp
-file { "/tmp/school":
-    ensure  => "file",
-    mode    => "0744",
-    owner   => "www-data",
-    group   => "www-data",
-    content => "I love Puppet",
+file { '/tmp/school':
+    ensure  => 'file',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
