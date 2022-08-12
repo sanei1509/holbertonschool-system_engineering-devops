9c708cb4f42de74a387d2dfabe23c41d799e1e60
Santiago Neira <santiagoneira52@gmail.com>
2022-08-12 04:32:29 +0000

    try task 1, create a file with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Crear un archivo en /tmp
+file { "/tmp/school":
+    ensure  => "file",
+    mode    => "0744",
+    owner   => "www-data",
+    group   => "www-data",
+    content => "I love Puppet",
+}
