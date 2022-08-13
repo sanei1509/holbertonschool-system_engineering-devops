55306fad3b3597ae6663d85d1dbbc2240f19bdea
Santiago Neira <santiagoneira52@gmail.com>
2022-08-13 16:49:34 +0000

    try task adv

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # automatizar lo que hicimos en la task 2
 exec {'/etc/ssh/ssh_config':
-  command  => 'echo "	IdentityFile ~/.ssh/school" > /etc/ssh/ssh_config || echo "	PasswordAuthentication no" > /etc/ssh/ssh_config',
+  command  => 'echo "	IdentityFile ~/.ssh/school" > /etc/ssh/ssh_config && echo "	PasswordAuthentication no" > /etc/ssh/ssh_config',
   provider => 'shell'
 }
