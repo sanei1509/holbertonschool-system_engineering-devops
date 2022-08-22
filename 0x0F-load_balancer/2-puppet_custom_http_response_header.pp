2d7202b1f8542c7af5439f1548c5f56be44ff53d
Santiago Neira <santiagoneira52@gmail.com>
2022-08-22 19:06:02 +0000

    fix task 2 advanced

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,28 @@
 # automatizar la tareas realizada en la task 0
 
-#Execution
+#update
 exec {'update':
     command  => 'sudo apt-get -y update',
     provider => shell,
     before   => Exec['nginx'],
 }
 
-#Nginx Install
+#Instalamos nginx
 exec {'nginx':
     command  => 'sudo apt-get -y install nginx',
     provider => shell,
-    before  => Exec['custom header'],
+    before  => Exec['custom-header'],
 }
 
 #Trying to modify the header
-exec {'custom header':
+exec {'custom-header':
     provider => shell,
     command  => "sudo sed -i '/listen 80 default_server/a add_header X-Served-By ${hostname};' /etc/nginx/sites-enabled/default",
     before   => Exec['restart'],
 }
 
-#Restarting nginx
-exec {'restart':
+#Reiniciar nginx
+exec {'reiniciar-nginx':
     command  => 'sudo service nginx restart',
     provider => shell
 }
