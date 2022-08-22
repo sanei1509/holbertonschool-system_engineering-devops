dc4ed37e493b62be5a3e6ab1424565cc525d075e
Santiago Neira <santiagoneira52@gmail.com>
2022-08-22 17:50:56 +0000

    try task 2 puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,26 @@
+# automatizar la tareas realizada en la task 0
+
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+
+#instalar nginx
+package {'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+#intentar crear el header
+file_line { 'custom-header':
+  ensure  => present,
+    path  => '/etc/nginx/sites-available/default',
+    after => 'server {',
+  line    => " 	location / {
+	add_header X-Served-By ${hostname};",
+}
+
+exec {'reiniciar nginx':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
