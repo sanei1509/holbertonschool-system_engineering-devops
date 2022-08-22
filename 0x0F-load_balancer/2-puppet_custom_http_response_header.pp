0562009f29e7c3b1bb4ee7be7bd8063d9c2d2616
Santiago Neira <santiagoneira52@gmail.com>
2022-08-22 19:03:25 +0000

    automatize with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,28 @@
 # automatizar la tareas realizada en la task 0
 
-exec { 'update':
-  command  => 'sudo apt-get -y update',
-  provider => shell,
-  before   => Exec['install_Nginx'],
+#Execution
+exec {'update':
+    command  => 'sudo apt-get -y update',
+    provider => shell,
+    before   => Exec['nginx'],
 }
 
-# instalar nginx:
-exec {'install_Nginx':
-  command  => 'sudo apt-get -y install nginx',
-  provider => shell,
-  before   => Exec['custom_header'],
+#Nginx Install
+exec {'nginx':
+    command  => 'sudo apt-get -y install nginx',
+    provider => shell,
+    before  => Exec['custom header'],
 }
 
-exec { 'custom_header':
-  provider => shell,
-  command  => "sudo sed -i '/listen 80 default_server/a add_header X-Served-By ${hostname};' /etc/nginx/sites-enabled/default",
-  before   => Exec['reiniciar_nginx'],
+#Trying to modify the header
+exec {'custom header':
+    provider => shell,
+    command  => "sudo sed -i '/listen 80 default_server/a add_header X-Served-By ${hostname};' /etc/nginx/sites-enabled/default",
+    before   => Exec['restart'],
 }
 
-# reiniciar servidor nginx
-exec { 'reiniciar-nginx': # se puede poner cualquier nombre aca
-  command  => 'sudo service nginx restart', # Aca se le pasa el comando a ejecutar
-  provider => 'shell' # Se le indica donde se va a correr
-  }
+#Restarting nginx
+exec {'restart':
+    command  => 'sudo service nginx restart',
+    provider => shell
+}
