7e418fdc5d69a5c44726e63aaed0373f9265ce01
Santiago Neira <santiagoneira52@gmail.com>
2022-09-06 17:48:11 +0000

    try task one

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,9 +10,9 @@ try:
     id_user = sys.argv[1]
     # DATA fake en la web
     url_user = "https://jsonplaceholder.typicode.com/users/{}"\
-                .format(id_user)
+               .format(id_user)
     url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}"\
-                .format(id_user)    
+                .format(id_user)
     # get de la DATA
     user_data = requests.get(url_user)
     todos_data = requests.get(url_todos)
@@ -25,21 +25,21 @@ try:
     # Luego de que tengo los datos voy a trabajar la exportación
     # abrir el archivo con los permisos necesarios
 
-
     # Formato a imprimir en el csv
-    contenido = []
-    for dic in todos_json:
-        contenido.append(dic["userId"])
-        contenido.append(user_json["name"])
-        contenido.append(dic["completed"])
-        contenido.append(dic["title"])
-
     with open("{}.csv".format(user_json["id"]), 'w') as file:
         # escribo en formato csv dentro del archivo
-        write = csv.writer(file) 
+        write = csv.writer(file, quoting=csv.QUOTE_ALL)
+        contenido = []
 
-        # creo una fila en el archivo CSV
-        write.writerow(contenido)
+        for dic in todos_json:
+            contenido.append(dic["userId"])
+            contenido.append(user_json["name"])
+            contenido.append(dic["completed"])
+            contenido.append(dic["title"])
+            # creo una fila en el archivo CSV
+            write.writerow(contenido)
+            contenido = []
+
 
 except Exception as err:
     print("error en la recolección de datos {}".format(err))
