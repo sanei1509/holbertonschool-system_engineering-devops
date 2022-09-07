3a53f16f0d80dc220b82561ad1409bacc81da4bd
Santiago Neira <santiagoneira52@gmail.com>
2022-09-07 03:21:22 +0000

    list of the users and his/her tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -15,20 +15,23 @@ try:
 
     user_json = user_data.json()
     todos_json = todos_data.json()
-    
+
     with open("todo_all_employees.json", "w", newline='') as outfile:
         todos = {}
+        # itero sobre los usuarios
         for user in user_json:
             lista_tareas = []
+            # itero sobre las tareas
             for task in todos_json:
+                # (comparo) necesito ir almacenando usuario y respectivas tasks
                 if task["userId"] == user["id"]:
                     sub_dic = {}
                     sub_dic['username'] = user['username']
                     sub_dic['task'] = task['title']
                     sub_dic['completed'] = task["completed"]
                     lista_tareas.append(sub_dic)
+            # finalizo de analizar un usuario y almaceno con su lista de tareas
             todos[user["id"]] = lista_tareas
-    # luego de formatear todo como necesitabamos guardamos en el json file
     json.dump(todos, outfile)
 
 except Exception as err:
