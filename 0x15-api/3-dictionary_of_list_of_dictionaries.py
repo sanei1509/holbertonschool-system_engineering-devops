0bbd3987eabc80b02a3076b0da41575661d7ff12
Santiago Neira <santiagoneira52@gmail.com>
2022-09-07 03:16:17 +0000

    try task three

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,12 +2,10 @@
 """ Dictionary of list of dictionaries """
 if __name__ == "__main__":
     import requests
-    import sys
     import json
 
 
 try:
-    id_user = sys.argv[1]
     # DATA fake en la web
     url_user = "https://jsonplaceholder.typicode.com/users"
     url_todos = "https://jsonplaceholder.typicode.com/todos"
@@ -15,24 +13,23 @@ try:
     user_data = requests.get(url_user)
     todos_data = requests.get(url_todos)
 
-    DATA en json
     user_json = user_data.json()
     todos_json = todos_data.json()
-
-    dictionary = {}
-    lista_tareas = []
-    for task in todos_json:
-        sub_dic = {}
-        sub_dic['task'] = task['title']
-        sub_dic['completed'] = task['completed']
-        sub_dic['username'] = user_json["username"]
-        lista_tareas.append(sub_dic)
-    dictionary["{}".format(user_json)] = lista_tareas
-
-    print(dictionary)
-
-    with open("todo_all_employees.json", "w") as outfile:
-        json.dump(dictionary, outfile)
+    
+    with open("todo_all_employees.json", "w", newline='') as outfile:
+        todos = {}
+        for user in user_json:
+            lista_tareas = []
+            for task in todos_json:
+                if task["userId"] == user["id"]:
+                    sub_dic = {}
+                    sub_dic['username'] = user['username']
+                    sub_dic['task'] = task['title']
+                    sub_dic['completed'] = task["completed"]
+                    lista_tareas.append(sub_dic)
+            todos[user["id"]] = lista_tareas
+    # luego de formatear todo como necesitabamos guardamos en el json file
+    json.dump(todos, outfile)
 
 except Exception as err:
     print(err)
