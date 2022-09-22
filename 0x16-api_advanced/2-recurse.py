1b04f3371a40a8b244de36fa20d511fd967fbcba
Santiago Neira <santiagoneira52@gmail.com>
2022-09-22 02:14:56 +0000

    try task 2 of a recursive looking for

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,5 +7,30 @@ Return None sino se encuentran los mismos
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
-	
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    task 5 -> recursive looking for
+    """
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+        headers = requests.utils.default_headers()
+        headers.update({"User-Agent": "Mozila/5.0"})
+
+        res = requests.get(url, headers=headers, params={'after': after},
+                           allow_redirects=False)
+
+        content_json = res.json()
+        articles = content_json["data"]["children"]
+
+        for titulo in articles:
+            hot_list.append(titulo["data"]["title"])
+
+        after = content_json["data"]["after"]
+
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+
+        return hot_list
+    except Exception:
+        return None
